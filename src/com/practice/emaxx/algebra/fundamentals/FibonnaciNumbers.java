8043e98ef1b1eff610a5e46f2d389f75181e3f65
Varun Vats <vvats@mobileiron.com>
2018-11-23 23:57:13 +0530

    Fibonacci refactor code.

--- src/com/practice/emaxx/algebra/fundamentals/FibonnaciNumbers.java
+++ src/com/practice/emaxx/algebra/fundamentals/FibonnaciNumbers.java
@@ -19,17 +19,19 @@ public class FibonnaciNumbers {
      *
      * It is based on the fact that, if you know F(k) and F(k+1)
      * you can calculate F(2k) and F(2k+1) with these equations.
+     * But remember, if you have F(2k) + F(2k+1) = F(2K+2).
      *
-     * Example : with (1, 2) --> (2, 3) --> (4, 5) --> (8, 9) --> (16, 17)
+     * So, example : k = 0, then if you know F(0) and F(1), you can calculate
+     * F(2*0), F(2*0+1) and F(2*0+2), basically F(0), F(1) and F(2).
+     *
+     * Same if you know, k = 1, F(1) and F(2) then you can calculate F(2) and F(3).
+     *
+     * Example : with (0, 1) --> (1, 2) --> (2, 3) --> (4, 5) --> (8, 9) --> (16, 17)
      */
     private static Pair<Integer, Integer> fib(final long n) {
 
         if (n == 0) {
             return new Pair<>(0, 1);
-        }
-
-        if (n == 1) {
-            return new Pair<>(1, 1);
         }
 
         Pair<Integer, Integer> pair = fib(n >> 1);
@@ -40,9 +42,9 @@ public class FibonnaciNumbers {
          *
          * Once all the recursion has been done, function should return n, and n+1.
          *
-         * Here, n is odd, that is till should return 2k+1, and 2K+2 (2k sum 2k+1).
+         * Here, n is odd, that is, it should return 2k+1, and 2K+2 (2k sum 2k+1).
          *
-         * Has it been n = 16, ie. even, it should have just return the 2k, and 2k+1.
+         * Had it been n = 16, ie. even, it should have just return the 2k, and 2k+1.
          */
         int even = pair.getLeft() * (2 * pair.getRight() - pair.getLeft());
         int odd = pair.getRight() * pair.getRight() + pair.getLeft() * pair.getLeft();
